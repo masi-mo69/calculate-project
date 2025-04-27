0fe3b0f5b9051ca813dabfc3bce8b8bd78ac9300
Mohammadi <ms.mohammadi69@gmail.com>
2025-04-27 20:50:49 +0330

    add styles for project

--- src/component/ResultsTable/ResultsTable.js
+++ src/component/ResultsTable/ResultsTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from './ResultTable.module.css';
 
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -7,9 +8,9 @@ const formatter = new Intl.NumberFormat('en-US', {
     maximumFractionDigits: 2,
   });
 
-const ResultsTable = () => {
+const ResultsTable = (props) => {
     return (
-        <table className="result">
+        <table className={classes.result}>
         <thead>
           <tr>
             <th>Year</th>
--- src/component/UserInput/UserInput.js
+++ src/component/UserInput/UserInput.js
@@ -1,6 +1,5 @@
-import { tab } from "@testing-library/user-event/dist/tab";
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
+import classes from './UserInput.module.css';
 
 const initialUserInput = {
     'current-savings' : 10000,
@@ -32,8 +31,8 @@ const UserInput = (props) => {
     };
 
     return (
-        <form className="form" onSubmit={submitHandler}>
-        <div className="input-group">
+        <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes['input-group']}>
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
             <input 
@@ -56,7 +55,7 @@ const UserInput = (props) => {
                 }/>
           </p>
         </div>
-        <div className="input-group">
+        <div className={classes['input-group']}>
           <p>
             <label htmlFor="expected-return">
               Expected Interest (%, per year)
@@ -82,7 +81,7 @@ const UserInput = (props) => {
             />
           </p>
         </div>
-        <p className="actions">
+        <p className={classes.actions}>
           <button 
             type="reset" 
             className="buttonAlt" 
@@ -90,7 +89,7 @@ const UserInput = (props) => {
         >
             Reset
           </button>
-          <button type="submit" className="button">
+          <button type="submit" className={classes.button}>
             Calculate
           </button>
         </p>
